e92001e7efbf29b1afeb3c9340da5c96b8a2a5f0
Ostap Salo <jlumon211@gmail.com>
2022-05-31 23:02:30 +0300

    make build

--- src/index.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-const form = document.getElementById('my_form');
-
-form.addEventListener('submit', function handleSubmit(event) {
-  event.preventDefault();
-
-  form.reset();
-});
